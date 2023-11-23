aaa0b0c205558e25db644f8f9d5045c71ac78faa
mmarcosab <marcosab.vinicius@gmail.com>
2023-11-23 16:41:50 -0300

    add jpa, mysql e data class

---
+++ src/main/kotlin/com/example/kotlinstudies/data/PersonData.kt
@@ -0,0 +1,21 @@
+package com.example.kotlinstudies.data
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
+
+@Entity
+@Table(name = "person")
+data class PersonData (
+        @field:NotNull @Id @GeneratedValue(strategy=GenerationType.AUTO) val id: Int,
+        @field:NotEmpty @Size(min = 3, max = 80) val name: String,
+        @field:NotEmpty @Size(max = 12) val surname: String,
+        @field:NotNull val age: Int
+) {
+
+}
