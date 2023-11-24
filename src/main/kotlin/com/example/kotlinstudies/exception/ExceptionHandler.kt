ded074075934372abce8c15a579def95cc9c4753
mmarcosab <marcosab.vinicius@gmail.com>
2023-11-24 09:18:44 -0300

    add exception handler

--- src/main/kotlin/com/example/kotlinstudies/exception/ExceptionHandler.kt
+++ src/main/kotlin/com/example/kotlinstudies/exception/ExceptionHandler.kt
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
-
 @RestControllerAdvice
 class ExceptionHandler {
 
